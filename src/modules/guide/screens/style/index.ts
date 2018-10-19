3ba25b38ad67bcd12849563e3c819c6432f2432a
weifengzz <weifengzz@outlook.com>
2018-10-19 20:59:20 +0800

    android 打包配置

--- src/modules/guide/screens/style/index.ts
+++ src/modules/guide/screens/style/index.ts
@@ -3,10 +3,17 @@
  */
 import { ViewStyle } from 'react-native'
 
+/**
+ * 引导界面样式
+ * @interface
+ */
 export interface IGuideStyle {
   container: ViewStyle
 }
 
+/**
+ * 引导界面样式
+ */
 export const GuideStyle = {
   container: {
     flex: 1,
